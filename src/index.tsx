9f9fc17f19eafca4b7e4200757143faafb9d9fda
rinick <rinick@gmail.com>
2020-12-07 20:56:58 -0800

    fix typescript error

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+interface Feature {
+  width?: number;
+  height?: number;
+  left?: number;
+  top?: number;
+}
+
 interface Props {
-  children: React.ReactNode;
-  url: string,
-  name: string;
-  title: string;
-  features: object;
-  onUnload: Function;
-  onBlock: Function;
-  onOpen: Function;
-  center: 'parent' | 'screen';
-  copyStyles: boolean;
+  children?: React.ReactNode;
+  url?: string;
+  name?: string;
+  title?: string;
+  features?: Feature;
+  onUnload?: () => void;
+  onBlock?: () => void;
+  onOpen?: (w: Window) => void;
+  center?: 'parent' | 'screen';
+  copyStyles?: boolean;
 }
 
 interface State {
@@ -27,30 +34,28 @@ class NewWindow extends React.PureComponent<Props, State> {
   /**
    * NewWindow default props.
    */
-  static defaultProps = {
+  static defaultProps: Props = {
     url: '',
     name: '',
-    title: '',
-    features: { width: '600px', height: '640px' },
-    onBlock: null,
-    onOpen: null,
-    onUnload: null,
-    center: 'parent',
+    features: { width: 600, height: 600 },
     copyStyles: true,
   };
 
+  window: Window;
 
-  window: Window = null;
-  windowCheckerInterval = null;
+  windowCheckerInterval: any;
+
   released = false;
+
   container = document.createElement('div');
+
   state: State = { mounted: false };
 
   /**
    * The NewWindow function constructor.
    * @param {Object} props
    */
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
   }
 
@@ -82,26 +87,24 @@ class NewWindow extends React.PureComponent<Props, State> {
         'width and height window features must be present when a center prop is provided',
       );
     } else if (center === 'parent') {
-      features.left =
-        window.top.outerWidth / 2 + window.top.screenX - features.width / 2;
-      features.top =
-        window.top.outerHeight / 2 + window.top.screenY - features.height / 2;
+      features.left = window.top.outerWidth / 2 + window.top.screenX - features.width / 2;
+      features.top = window.top.outerHeight / 2 + window.top.screenY - features.height / 2;
     } else if (center === 'screen') {
       const screenLeft =
-        window.screenLeft !== undefined ? window.screenLeft : window.screen['left'];
+        window.screenLeft !== undefined ? window.screenLeft : (window.screen as any).left;
       const screenTop =
-        window.screenTop !== undefined ? window.screenTop : window.screen['top'];
+        window.screenTop !== undefined ? window.screenTop : (window.screen as any).top;
 
       const width = window.innerWidth
         ? window.innerWidth
         : document.documentElement.clientWidth
-          ? document.documentElement.clientWidth
-          : window.screen.width;
+        ? document.documentElement.clientWidth
+        : window.screen.width;
       const height = window.innerHeight
         ? window.innerHeight
         : document.documentElement.clientHeight
-          ? document.documentElement.clientHeight
-          : window.screen.height;
+        ? document.documentElement.clientHeight
+        : window.screen.height;
 
       features.left = width / 2 - features.width / 2 + screenLeft;
       features.top = height / 2 - features.height / 2 + screenTop;
@@ -138,7 +141,7 @@ class NewWindow extends React.PureComponent<Props, State> {
     } else {
       // Handle error on opening of new window.
       if (typeof onBlock === 'function') {
-        onBlock(null);
+        onBlock();
       } else {
         console.warn('A new window could not be opened. Maybe it was blocked.');
       }
@@ -171,7 +174,7 @@ class NewWindow extends React.PureComponent<Props, State> {
     const { onUnload } = this.props;
 
     if (typeof onUnload === 'function') {
-      onUnload(null);
+      onUnload();
     }
   }
 }
@@ -188,48 +191,49 @@ class NewWindow extends React.PureComponent<Props, State> {
  * @private
  */
 
-function copyStyles(source, target) {
+function copyStyles(source: Document, target: Document) {
   Array.from(source.styleSheets).forEach(styleSheet => {
     // For <style> elements
     let rules;
-    try {
-      rules = styleSheet.cssRules;
-    } catch (err) {
-      console.error(err);
-    }
-    if (rules) {
-      const newStyleEl = source.createElement('style');
 
-      // Write the text of each rule into the body of the style element
-      Array.from(styleSheet.cssRules).forEach(cssRule => {
-        const { cssText, type } = cssRule;
-        let returnText = cssText;
-        // Check if the cssRule type is CSSImportRule (3) or CSSFontFaceRule (5) to handle local imports on a about:blank page
-        // '/custom.css' turns to 'http://my-site.com/custom.css'
-        if ([3, 5].includes(type)) {
-          returnText = cssText
-            .split('url(')
-            .map(line => {
-              if (line[1] === '/') {
-                return `${line.slice(0, 1)}${
-                  window.location.origin
-                }${line.slice(1)}`;
-              }
-              return line;
-            })
-            .join('url(');
-        }
-        newStyleEl.appendChild(source.createTextNode(returnText));
-      });
-
-      target.head.appendChild(newStyleEl);
-    } else if (styleSheet.href) {
+    if (styleSheet.href) {
       // for <link> elements loading CSS from a URL
       const newLinkEl = source.createElement('link');
 
       newLinkEl.rel = 'stylesheet';
       newLinkEl.href = styleSheet.href;
       target.head.appendChild(newLinkEl);
+    } else {
+      try {
+        rules = styleSheet.cssRules;
+      } catch (err) {
+        // can't access crossdomain rules
+      }
+      if (rules) {
+        const newStyleEl = source.createElement('style');
+
+        // Write the text of each rule into the body of the style element
+        Array.from(styleSheet.cssRules).forEach(cssRule => {
+          const { cssText, type } = cssRule;
+          let returnText = cssText;
+          // Check if the cssRule type is CSSImportRule (3) or CSSFontFaceRule (5) to handle local imports on a about:blank page
+          // '/custom.css' turns to 'http://my-site.com/custom.css'
+          if ([3, 5].includes(type)) {
+            returnText = cssText
+              .split('url(')
+              .map(line => {
+                if (line[1] === '/') {
+                  return `${line.slice(0, 1)}${window.location.origin}${line.slice(1)}`;
+                }
+                return line;
+              })
+              .join('url(');
+          }
+          newStyleEl.appendChild(source.createTextNode(returnText));
+        });
+
+        target.head.appendChild(newStyleEl);
+      }
     }
   });
 }
@@ -241,7 +245,7 @@ function copyStyles(source, target) {
  * @private
  */
 
-function toWindowFeatures(obj) {
+function toWindowFeatures(obj: any) {
   return Object.keys(obj)
     .reduce((features, name) => {
       const value = obj[name];
